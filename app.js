af07c90a849b24e354c01efefc44f29e960cf7bc
Mohamad Nadimi <mmdseq@gmail.com>
2021-10-08 04:44:22 +0330

    firr

--- app.js
+++ app.js
@@ -5,66 +5,81 @@ const timeEl = document.querySelector(".time");
 const dateEl = document.querySelector(".date");
 const toggleEl = document.querySelector(".toggle");
 
-const days = ["Sunday","Monday","Tuesday","Wendsday","Thursday","Friday","Saturday"];
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wendsday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Now","Dec"];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Now",
+  "Dec",
+];
 
+toggleEl.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
 
-
-
-
-toggleEl.addEventListener('click',(e)=>{
-  const html = document.querySelector('html');
-
-  if(e.target.innerText === 'Dark mode'){
-    e.target.innerText='Light mode'
-    html.classList.add('dark')
-  }else{
-    e.target.innerText='Dark mode'
-    html.classList.remove('dark')
+  if (e.target.innerText === "Dark mode") {
+    e.target.innerText = "Light mode";
+    html.classList.add("dark");
+  } else {
+    e.target.innerText = "Dark mode";
+    html.classList.remove("dark");
   }
+});
 
-})
+setTime();
 
-setTime()
+setInterval(() => setTime(), 1000);
 
-setInterval(()=> setTime(),1000)
-
-
-function setTime(){
-
+function setTime() {
   const time = new Date();
 
   const hour = time.getHours();
-  const hoursForClock=hour % 12
+  const hoursForClock = hour % 12;
   const minute = time.getMinutes();
   const second = time.getSeconds();
   const date = time.getDate();
   const day = time.getDay();
-  const month= time.getMonth();
+  const month = time.getMonth();
 
-
-  hourEl.style.transform=`translate(0,-100%) rotate(${scale(hoursForClock,0,11,0,360)}deg)`;
-  minuteEl.style.transform=`translate(0,-100%) rotate(${scale(minute,0,59,0,360)}deg)`;
-  secondEl.style.transform=`translate(0,-100%) rotate(${scale(second,0,59,0,360)}deg)`;
-
-
-  
-
+  hourEl.style.transform = `translate(0,-100%) rotate(${scale(
+    hoursForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`;
+  minuteEl.style.transform = `translate(0,-100%) rotate(${scale(
+    minute,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+  secondEl.style.transform = `translate(0,-100%) rotate(${scale(
+    second,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
 }
 
-
-
-
-
-
-
-function scale (number, inMin, inMax, outMin, outMax) {
-    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 }
-
-
-
-
-
-
